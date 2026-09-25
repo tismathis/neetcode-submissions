c2d21ff72b1287f678c975fd72a86814e9d5c8dc
tismathis <mathis.muhle@gmail.com>
2026-09-25 23:56:36 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+       profit = 0
+
+       i = 0 
+       j = 1 
+
+       while i != j : 
+            futureProfit = prices[j-1] - prices[i]
+            if profit < futureProfit :
+                profit = futureProfit 
+            if prices[i] < prices[i-1] :
+                i +=1
+            elif prices[j] > prices[j-1] :
+                j +=1
+            else :
+                j +=1 
+                if (j == len(prices)-1) :
+                    i +=1 
+            return profit 
