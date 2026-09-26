41e3507bcc12ea105102b68d248bf12fa8202fda
tismathis <mathis.muhle@gmail.com>
2026-09-26 22:57:43 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxim = 0
+
+        for i in range (len(s)) :
+            for j,val in enumerate(s) :
+                HashMAP = {} 
+                if val not in HashMAP :
+                    HashMAP[val] = 1
+                else :
+                    HashMAP[val] +=1
+                
+                length = j-i + 1 
+                maxFreq = max(HashMAP.values())
+                rep = length - maxFreq
+                if rep < k :
+                    maxim = max(maxim,length) 
+        return maxim
+
+                
